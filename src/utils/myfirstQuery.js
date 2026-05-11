3f692ec3155cebe61bb1560ebdbf5c4eadcfcc63
Àsthildur Greta Alfredsdóttir <eldvakinn@hotmail.com>
2026-05-11 10:26:04 +0200

    Add myFirstQuery for fetching blog titles

--- src/utils/myfirstQuery.js
+++ src/utils/myfirstQuery.js
@@ -1,5 +1,5 @@
 import { gql } from "graphql-request";
-
+// inde imellem `` er det man skal skrive i graphql, på hygraph siden i api playground
 export const myFirstQuery = gql `
 query {
   blogs {
